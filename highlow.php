71c08264ca5026718589e5067bf669f4a595c66e
haydogsup <joshua.r.mays@gmail.com>
2014-08-14 11:24:31 -0500

    first commit of high-low game

---
+++ highlow.php
@@ -0,0 +1,28 @@
+<?php
+
+// Set random number from 1 - 100 for user to guess.
+$answer = rand(1,100);
+
+// Start do-while loop asking user to guess the $answer.
+do {
+	// fwrite to stdout asking the user to guess.
+	fwrite(STDOUT, 'Guess? ');
+	// save user's guess with fgets(stdin) as $guess
+	$guess = trim(fgets(STDIN));
+
+	if ($guess == $answer) {
+		// User's guess was correct, congratulate them.
+		fwrite(STDOUT, "GOOD GUESS!\n");
+	}
+	elseif ($guess > $answer) {
+		// If $guess is higher than $answer tell user to guess lower.
+		fwrite(STDOUT, "LOWER\n");
+	}
+	else {
+		// If $guess is lower than $answer tell user to guess higher.
+		fwrite(STDOUT, "HIGHER\n");
+	}
+} while ($guess != $answer); // Check if $guess is not equal to $answer, if true
+							 // run the loop again. If not, end the game. 
+?>
+
